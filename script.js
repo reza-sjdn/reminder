d773e34782c2a836412c7cc91a22782b12c72c9f
reza-sjdn <rezasajjadian22@gmail.com>
2023-09-22 15:06:42 +0330

    changed the deadline to my possible death time

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-const deadline = new Date("2023-9-25");
-const diff = deadline.getTime() - Date.now();
+setInterval(findTimeLeft, 1000);
 
-const daysLeft = diff / 86400000;
-document.getElementById("days").innerHTML = Math.floor(daysLeft);
 
-const progress = (daysLeft / 45) * 100;
-document.getElementById("indicator").style.height = `${progress}%`;
+function findTimeLeft () {
+    const deadline = new Date("2081-12-11");
+    const diff = deadline.getTime() - Date.now();
+
+    let daysLeft = diff / 86400000;
+    document.getElementById("year").innerHTML = fill(Math.floor(daysLeft / 365));
+    daysLeft %= 365;
+    
+    document.getElementById("month").innerHTML = fill(Math.floor(daysLeft / 30));
+    daysLeft %= 30;
+    
+    document.getElementById("day").innerHTML = fill(Math.floor(daysLeft));
+    let secondsLeft = (daysLeft - Math.floor(daysLeft)) * 86400;
+    
+    document.getElementById("hour").innerHTML = fill(Math.floor(secondsLeft / 3600));
+    secondsLeft %= 3600;
+    
+    document.getElementById("minute").innerHTML = fill(Math.floor(secondsLeft / 60));
+    secondsLeft %= 60;
+    
+    document.getElementById("second").innerHTML = fill(Math.floor(secondsLeft));
+    
+    
+    const progress = (diff / (80 * 365 * 86400000)) * 100;
+    document.getElementById("indicator").style.height = `${progress}%`;
+}
+
+
+function fill (num) {
+    return (num < 10) ? ('0' + num) : num;
+}
